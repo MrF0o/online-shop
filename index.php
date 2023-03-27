9deef91037edfa79d3b9c6040b6da36cc8d8e674
MrF0o <fathihelmi36@gmail.com>
2023-03-27 21:16:45 +0100

    barchaa hajet

--- index.php
+++ index.php
@@ -1,42 +1,62 @@
 <?php include(__DIR__ . '/header.php') ?>
 
+<div class="hero">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container d-flex h-100 flex-column justify-content-center">
+            <div>
+                <h1 class="display-3 fw-bold text-capitalize">profitez c'est l'occasion ou jamais ne ratez pas !</h1>
+                <p class="fw-normal"> Repudiandae quidem dicta tenetur illo rerum eveniet veritatis sit mollitia perspiciatis autem,<br> nisi libero commodi deleniti nemo quisquam amet consequatur accusamus harum.</p>
+                <button class="btn btn-lg btn-outline-light">Shop Now</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container mt-5 d-flex flex-column align-items-center">
-    <div id="featuredProductsIndicator" class="carousel slide col-md-10 col-lg-12  col-12" data-bs-ride="carousel">
+    <!-- <div id="featuredProductsIndicator" class="carousel slide col-md-6 shadow" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Product 1"></button>
             <button type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide-to="1" aria-label="Product 2"></button>
             <button type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide-to="2" aria-label="Product 4"></button>
         </div>
-        <div class="carousel-inner rounded shadow">
+        <div class="carousel-inner rounded">
             <div class="carousel-item active">
-                <div class="bg-secondary d-flex justify-content-center align-items-center">
+                <div class="">
 
-                    <div class="slide-content"></div>
+                    <img class="slider-img" src="images/image.png">
 
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="bg-secondary d-flex justify-content-center align-items-center">
-                    <p class="text-white h4 py-5">nchlh njibou taswira article 2 lhne </p>
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="d-flex justify-content-center align-items-center">
+
+                        <img class="slider-img" src="images/2.png">
+
+                    </div>
                 </div>
             </div>
             <div class="carousel-item">
-                <div class="bg-secondary d-flex justify-content-center align-items-center">
-                    <p class="text-white h4 py-5">nchlh njibou taswira article 3 lhne </p>
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="bg-secondary d-flex justify-content-center align-items-center">
+
+                        <img class="slider-img" src="images/3.png">
+
+                    </div>
                 </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide="prev">
+        <button class="carousel-control-prev rounded" type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide="next">
+        <button class="carousel-control-next rounded" type="button" data-bs-target="#featuredProductsIndicator" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
-    </div>
+    </div> -->
 
-    <div class="products card mt-5">
+    <div class="products card mt-5 w-100">
         <div class="products-header card-header">
             <h4 class="h4 fw-4">Nos séléctions</h4>
         </div>
@@ -96,37 +116,41 @@
                     </div>
                 </div>
             </div>
-
-            <div class="card col-12 col-md w-100 p-0" style="width:18rem;">
-                <img style="max-height:16rem;object-fit:cover" src="./images/1.png" class="card-img-top" style alt="...">
-                <div class="card-body">
-                    <h3 class="fs-4 fw-4 text-center card-title">test produit</h3>
-                    <div class="text-center">
-                        <span class="text-decoration-line-through">230DT</span>
-                        <span style="font-weight:500">230DT</span>
-                    </div>
-                    <div class="pt-4 text-center">
-                        <button class="btn btn-outline-dark">Ajouter au pannier</button>
-                    </div>
-                </div>
-            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="#" class="btn btn-outline-dark m-2">Afficher tous</a>
         </div>
     </div>
 
-    <div class="about row justify-content-center my-5 p-3">
-        <div class="col-12 col-md-8 col-lg-7">
-            <h4>Qui sommes-nous ?</h4>
-        </div>
-        <div class="d-flex bg-light col-12 col-md-8 col-lg-7 rounded shadow flex-column flex-md-row">
-            <div class="p-2 d-flex d-md-block justify-content-center">
-                <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon "><a href="#"><i class="fab fa-facebook text-dark"></i></a></div>
-                <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-instagram text-dark"></i></a></div>
-                <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-whatsapp text-dark"></i></a></div>
-                <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-youtube text-dark"></i></a></div>
+    <div class="about w-100 row justify-content-center my-5">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <div class="col-12 col-md-8 col-lg-7">
+                    <h4>Qui sommes-nous ?</h4>
+                </div>
+                <div class="d-flex bg-light rounded shadow flex-column flex-md-row">
+                    <div class="p-2 d-flex d-md-block justify-content-center">
+                        <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon "><a href="#"><i class="fab fa-facebook text-dark"></i></a></div>
+                        <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-instagram text-dark"></i></a></div>
+                        <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-whatsapp text-dark"></i></a></div>
+                        <div class="my-md-2 mx-2 p-1 text-center fs-5 hover-icon"><a href="#"><i class="fab fa-youtube text-dark"></i></a></div>
+                    </div>
+                    <div class="p-2">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis a ducimus quis odit natus praesentium corrupti iure, voluptate tempora modi blanditiis perferendis numquam id sint earum mollitia aliquid quam! Temporibus?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis a ducimus quis odit natus praesentium corrupti iure, voluptate tempora modi blanditiis perferendis numquam id sint earum mollitia aliquid quam! Temporibus?</p>
+                    </div>
+                </div>
             </div>
-            <div class="p-2">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis a ducimus quis odit natus praesentium corrupti iure, voluptate tempora modi blanditiis perferendis numquam id sint earum mollitia aliquid quam! Temporibus?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis a ducimus quis odit natus praesentium corrupti iure, voluptate tempora modi blanditiis perferendis numquam id sint earum mollitia aliquid quam! Temporibus?</p>
+            <div class="d-flex col-12 col-md-6 flex-column justify-content-start">
+                <div class="fw-semibold py-3 text-uppercase">
+                    Nous Acceptons
+                </div>
+                <div class="payments d-flex">
+                    <div class="visa m-1 ml-0"></div>
+                    <div class="mastercard m-1"></div>
+                    <div class="amex m-1"></div>
+                    <div class="discover m-1"></div>
+                </div>
             </div>
         </div>
     </div>
--- modif-adresse.php
+++ modif-adresse.php
@@ -1,11 +1,33 @@
 <?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include 'config.php';
+
+// kn mahouch logged in aaml redirect lel login
+if (isset($_SESSION['login'])) {
+    // grab the current logged in member
+    $email = $_SESSION['login'];
+    $sql = "SELECT * FROM clients WHERE email='$email'";
+    $res = mysqli_query($db, $sql);
+    $membre = mysqli_fetch_assoc($res);
+
+
+    // kif yclicki ala mise a jours
+    if (isset($_POST['update'])) {
+        // ... TODO
+    }
+} else {
+    header('Location: login.php');
+}
 
 ?>
 
 <?php include 'header.php' ?>
 
 <div class="container h-100 d-flex justify-content-center align-items-center">
-    <div class="card col-md-5 bg-light">
+    <div class="card  col col-md-5 bg-light">
         <form class="card-body" action="" method="POST">
             <div class="form-group mt-2">
                 <label for="addr_line1">Addresse ligne 1<code>*</code></label>
@@ -18,9 +40,11 @@
             </div>
 
             <div class="form-group mt-2">
-                <label for="code_postal">Code postal</label>
+                <label for="code_postal">Code postal<code>*</code></label>
                 <input type="code_postal" name="code_postal" class="form-control" id="code_postal" placeholder="Entrer une code">
             </div>
+
+            <small>les champs marqués avec <code>*</code> sont obligatoires</small>
 
             <div class="mt-3">
                 <button type="submit" name="update" class="btn btn-outline-dark">Mise a jour d'adresse</button>
