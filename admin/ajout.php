2697cf2775dff721468bdd55578ff84b259745d1
MrF0o <fathihelmi36@gmail.com>
2023-03-29 22:00:04 +0100

    ajout des categories w articles

--- admin/ajout.php
+++ admin/ajout.php
@@ -4,10 +4,56 @@ if (!isset($_SESSION)) {
     session_start();
 }
 
-// ken mch logged in redirecti lel login
+include '../config.php';
+
+$msg = [];
+
+// ken mch logged in wla mahouch admin redirecti lel login
 if (isset($_SESSION['login']) && isset($_SESSION['is_admin'])) {
+
+    if (isset($_POST['add_cat'])) {
+        $msg['type'] = 'cat';
+        if (!empty($_POST['cat_title'])) {
+            $libelle = mysqli_escape_string($db, $_POST['cat_title']);
+
+            $query = "INSERT INTO categories(label) VALUES('$libelle')";
+            mysqli_query($db, $query);
+
+            $msg['success'] = true;
+        } else {
+            $msg['success'] = false;
+            $msg['msg'] = 'Libllé est vide!';
+        }
+    }
+
+    if (isset($_POST['add_prod'])) {
+        $msg['type'] = 'prod';
+        if (!empty($_POST['category']) && !empty($_POST['product_title']) && !empty($_POST['product_desc'])) {
+            $title = mysqli_escape_string($db, $_POST['product_title']);
+            $desc = mysqli_escape_string($db, $_POST['product_desc']);
+            $cat_id = mysqli_escape_string($db, $_POST['category']);
+
+            $query = "INSERT INTO article(title, description, category_id) VALUES('$title', '$desc', '$cat_id')";
+            mysqli_query($db, $query);
+
+            $msg['success'] = true;
+        } else {
+            $msg['success'] = false;
+            $msg['msg'] = 'Au moins un des champs est vide.';
+        }
+    }
 } else {
     header('Location: login.php');
+}
+
+// ken bch yajouti produit donc lazem njibou liste 
+// des categories bech naamlouha f select dropdown
+if ($_GET['type'] == 'produit') {
+    $sql = "SELECT * FROM categories";
+    $res = mysqli_query($db, $sql);
+
+    // nvm the naming :) !!
+    $cats = mysqli_fetch_all($res, MYSQLI_ASSOC);
 }
 
 
@@ -20,18 +66,94 @@ if (isset($_SESSION['login']) && isset($_SESSION['is_admin'])) {
 <main class="main-wrapper">
     <?php include 'navbar.php' ?>
     <div class="container mt-3">
+
         <?php if ($_GET['type'] == 'produit') : ?>
 
-            <h3>Ajouter un produit</h3>
+            <?php if (count($msg)) : ?>
+                <div class="col-md-6 mt-3">
+                    <?php if ($msg['success']) : ?>
+                        <p class="alert alert-success">Produit ajouté avec succée.</p>
+                    <?php else : ?>
+                        <p class="alert alert-danger"><?php echo $msg['msg'] ?></p>
+                    <?php endif ?>
+                </div>
+            <?php endif ?>
+
+            <h3>Ajouter un article</h3>
+
+            <div class="col-md-6 mt-3">
+                <form action="" method="POST">
+
+                    <div class="input-style-2">
+                        <label for="product_title">Titre de l'article</label>
+                        <input type="text" name="product_title" id="product_title" class="form-control" placeholder="Titre de l'article">
+                    </div>
+                    <div class="input-style-2">
+                        <label for="product_desc">Description de l'article</label>
+                        <textarea type="text" name="product_desc" id="product_desc" class="form-control" rows="4" placeholder="Description de l'article"></textarea>
+                    </div>
+                    <div class="select-style-1">
+                        <label>Category</label>
+                        <div class="select-position">
+                            <select name="category">
+                                <option value="">Selectionner une categorie</option>
+                                <?php if (isset($cats)) : ?>
+                                    <?php foreach ($cats as $cat) : ?>
+                                        <option value="<?php echo $cat['id'] ?>"><?php echo $cat['label'] ?></option>
+                                    <?php endforeach ?>
+                                <?php endif ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="input-style-2">
+                        <button name="add_prod" type="submit" class="btn btn-primary">Ajouter</button>
+                    </div>
+
+                </form>
+
+                <blockquote class="blockquote rounded p-1" style="border-left: 3px solid orange">
+                    TODO: taswira mtaa l'article
+                </blockquote>
+            </div>
 
         <?php endif ?>
 
         <?php if ($_GET['type'] == 'categorie') : ?>
+            <?php if (count($msg)) : ?>
+                <div class="col-md-6 mt-3">
+                    <?php if ($msg['success']) : ?>
+                        <p class="alert alert-success">Categorie ajouté avec succée.</p>
+                    <?php else : ?>
+                        <p class="alert alert-danger"><?php echo $msg['msg'] ?></p>
+                    <?php endif ?>
+                </div>
+            <?php endif ?>
 
             <h4>Ajouter une Categorie</h4>
+
+            <div class="col-md-6 mt-3">
+                <form action="" method="POST">
+
+                    <div class="input-style-2">
+                        <label for="cat_title">Libellé de categorie</label>
+                        <input type="text" name="cat_title" id="cat_title" class="form-control" placeholder="Libellé de categorie">
+                    </div>
+                    <div class="input-style-2">
+                        <button name="add_cat" type="submit" class="btn btn-primary">Ajouter</button>
+                    </div>
+                </form>
+            </div>
 
         <?php endif ?>
     </div>
 </main>
+
+<script>
+    // ala 5ater lpage taamel resubmit lel form
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
 
 <?php include 'footer.php' ?>
